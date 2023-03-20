8980b2f2931fabf03656d9e7f8111c454e5905f3
Team.Quick.Notes <quicknotes@selene.hud.ac.uk>
2023-03-20 07:56:28 +0000

    Your commit message here

--- Testing/main.php
+++ Testing/main.php
@@ -3,7 +3,6 @@
     session_start();
     include("connection.php");
 
-    // Form handling
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
         if (isset($_POST["submit"])) {
             $title = $_POST["title"];
@@ -41,101 +40,117 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+
+ 
+
+
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Notetaking App</title>
     <link rel="stylesheet" href="style2.css">
     <link rel="stylesheet" href="stylecolumn.css" />
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" rel="stylesheet" />
+    <link
+      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css"
+      rel="stylesheet"
+    />
     <script src="https://cdn.ckeditor.com/4.14.1/standard/ckeditor.js"></script>
-    <style>
-        /* Add custom styles here */
-        .main-content {
-            width: 80%;
-            margin: 0 auto;
-            padding: 2rem;
-        }
-        
-        form {
-            display: flex;
-            flex-direction: column;
-            gap: 1rem;
-        }
-        
-        label {
-            font-weight: bold;
-        }
-        
-        input[type="text"],
-        input[type="submit"],
-        textarea {
-            width: 100%;
-            padding: 0.5rem;
-            font-size: 1rem;
-            margin-bottom: 1rem;
-        }
-        
-        input[type="submit"] {
-            cursor: pointer;
-            background-color: var(--primary-clr);
-            color: white;
-            border: none;
-        }
-        
-        input[type="submit"]:hover {
-            background-color: var(--hover-clr);
-        }
-        
-        .notes-list {
-            list-style: none;
-            padding: 0;
-        }
-        
-        .notes-list li {
-            padding: 1rem;
-            border: 1px solid #ccc;
-            margin-bottom: 1rem;
-        }
-    </style>
-</head>
-<body>
-    <!-- Sidebar -->
-    <?php include 'sidebar.php'; ?>
 
-    <!-- Main Content -->
+</head>
+<body>    <div class="container">
+      <ul class="link-items">
+        <li class="link-item top">
+          <a href="#" class="link">
+            <img src="https://www.35mmc.com/wp-content/uploads/2021/10/00-logo.png" alt="" />
+            <span style="--i: 9">
+              <h4>Quick-Notes</h4>
+            </span>
+          </a>
+        </li>
+        <li class="link-item active">
+          <a href="#" class="link">
+            <ion-icon name="home-outline"></ion-icon>
+            <span style="--i: 1">Notes</span>
+          </a>
+        </li>
+        <li class="link-item">
+    <a href="search.php" class="link">
+        <ion-icon name="search-outline"></ion-icon>
+        <span style="--i: 4">Search</span>
+    </a>
+</li>
+
+        <li class="link-item">
+          <a href="#" class="link"
+            ><ion-icon name="layers-outline"></ion-icon>
+            <span style="--i: 3">projects</span>
+          </a>
+        </li>
+        <li class="link-item">
+          <a href="#" class="link">
+            <ion-icon name="checkbox-outline"></ion-icon
+            ><span style="--i: 5">tasks</span>
+          </a>
+        
+        <li class="link-item">
+          <a href="#" class="link">
+            <ion-icon name="cog-outline"></ion-icon>
+            <span style="--i: 6">settings</span>
+          </a>
+        </li>
+
+        <li class="link-item dark-mode">
+          <a href="#" class="link">
+            <ion-icon name="moon-outline"></ion-icon>
+            <span style="--i: 8">dark mode</span>
+          </a>
+        </li>
+      </ul>
+    </div>
+    
+    <script
+      type="module"
+      src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"
+    ></script>
+    <script
+      nomodule
+      src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"
+    ></script>
+    <script src="scriptcolumn.js"></script>
+
     <div class="main-content">
-        <h2>Quick Notes</h2>
+        <h2>Notetaking App</h2>
         <form method="post" action="main.php">
             <label for="title">Title:</label>
-            <id="title" name="title" required>
+            <input type="text" id="title" name="title">
+
             <label for="note">Note:</label>
-        <textarea id="note" name="note" required></textarea>
-        <script>
-            CKEDITOR.replace('note');
-        </script>
+            <textarea id="note" name="note"></textarea>
+            <script>
+                CKEDITOR.replace('note');
+            </script>
 
-        <input type="submit" value="Save Note" name="submit">
-    </form>
-    <form method="post" action="main.php">
-        <input type="text" id="search" name="search" placeholder="Search notes...">
-        <input type="submit" value="Search" name="submit_search">
-    </form>
+            <input type="submit" value="Save Note" name="submit">
+        </form>
+        <!-- <form method="post" action="main.php">
+            <input type="text" id="search" name="search">
+            <input type="submit" value="Search" name="submit_search">
+        </form> -->
 
-    <?php if (isset($search_result)): ?>
-        <h3>Search Results:</h3>
-        <ul class="notes-list">
-            <?php foreach ($search_result as $note): ?>
-                <li>
-                    <h4><?= htmlspecialchars($note['title']); ?></h4>
-                    <p><?= htmlspecialchars(substr(strip_tags($note['note']), 0, 100)) . '...'; ?></p>
-                    <a href="view_note.php?id=<?= $note['id']; ?>">Read More</a>
-                </li>
-            <?php endforeach; ?>
-        </ul>
-    <?php endif; ?>
-</div>
+        <?php if (isset($search_result)): ?>
+            <h3>Search Results:</h3>
+            <ul class="notes-list">
+                <?php foreach ($search_result as $note): ?>
+                    <li>
+                        <h4><?= htmlspecialchars($note['title']); ?></h4>
+                        <p><?= htmlspecialchars(substr(strip_tags($note['note']), 0, 100)) . '...'; ?></p>
+                    </li>
+                <?php endforeach; ?>
+            </ul>
+        <?php endif; ?>
+    </div>
 
-<script src="javascript2.js"></script>
+    <script src="javascript2.js"></script>
 </body>
 </html>
